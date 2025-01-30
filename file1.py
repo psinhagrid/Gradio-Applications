c6220f867d756f71633299059c532400a4f327d4
psinhagrid <158988758+psinhagrid@users.noreply.github.com>
2025-01-30 10:03:33 +0530

    new_functions

--- file1.py
+++ file1.py
@@ -1,6 +1,19 @@
 # This is a demo file. 
-print ("This is test 1")
-print (".")
-print (".")
-print (".")
-print ("This is the end of file")
+def func_1(count):
+    count += 1
+    func_2(count)
+
+def func_2(count):
+    count += 1
+    func_3(count)
+
+def func_3(count):
+    count += 1
+    func_4(count)
+
+def func_4(count):
+    count += 1
+    print ("DONE")
+
+count = 0
+func_1(count)
